5c29f44468478d199c6a1f29f597da965466e329
NPGonzalez <naypili@gmail.com>
2020-02-06 17:00:35 -0300

    Lectura de ruta desde línea de comando

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-module.exports = () => {
+//module.exports = () => {
   // ...
-};
+  //}
+
+console.log("Ingresa ruta");
+let pathToken = process.argv[2];
+if (pathToken){
+  const readPath = (path) =>{
+    console.log(pathToken);
+  };
+  readPath(pathToken);
+}else{
+  console.log("No ingrese ruta vacía");
+}

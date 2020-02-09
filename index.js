5d4fefdc677348a65d90097b5f58cd3e47af3324
NPGonzalez <naypili@gmail.com>
2020-02-09 19:28:54 -0300

    Validación de argumento de path y concatenación de ruta relativa con archivo .md a analizar

--- index.js
+++ index.js
@@ -1,17 +1,107 @@
-//module.exports = () => {
-  // ...
-  //}
-//LECTURA DE RUTA DESDE LÍNEA DE COMANDO
-console.log("Ingresa ruta");
-let pathToken = process.argv[2];
-if (pathToken){
+// //module.exports = () => {
+//   // ...
+//   //}
+
+//ALMACENAMIENTO DE PATH
+let pathAbsRel = process.argv[2];
+// FUNCIÓN mdLinks(path)
+const mdlinks = (pathToken) =>{
+// VALIDACIÓN DE PATH
+if(pathToken === undefined){
+console.log('ingrese ruta')}
+else{  
+//ALMACENAMIENTO DEL D
+let relativePath = process.cwd();
+//TRANSFORMACIÓN A RUTA ABSOLUTA
+let absolutePathToken = relativePath + '\\' + pathToken;
   const readPath = (path) =>{
-    console.log(pathToken);
+//SE MUESTRA RUTA ABSOLUTA...
+    console.log(path);
   };
-  readPath(pathToken);
-}else{
-  console.log("No ingrese ruta vacía");
+  readPath(absolutePathToken);
+};
 }
+
+mdlinks(pathAbsRel);
+
 // LECTURA CURRENT WORKING DIRECTORY
-let data = process.cwd();
-console.log(data);
+// let data = process.cwd();
+// console.log(data);
+
+// const https = require('https');
+// https.get('https://github.com/NPGonzalez/SCL012-Cipher/blob/master/README.md', (res) => {
+//   const { statusCode } = res;
+//   const contentType = res.headers['content-type'];
+
+//   let error;
+//   if (statusCode !== 200) {
+//     error = new Error('Request Failed.\n' +
+//                       `Status Code: ${statusCode}`);}
+//   // } else if (!/^application\/json/.test(contentType)) {
+//   //   error = new Error('Invalid content-type.\n' +
+//   //                     `Expected application/json but received ${contentType}`);
+//   // }
+//   if (error) {
+//     console.error(error.message);
+//     // Consume response data to free up memory
+//     res.resume();
+//     return;
+//   }
+// });
+// //   res.setEncoding('utf8');
+// //   let rawData = '';
+// //   res.on('data', (chunk) => { rawData += chunk; });
+// //   res.on('end', () => {
+// //     try {
+// //       const parsedData = JSON.parse(rawData);
+// //       console.log(parsedData);
+// //     } catch (e) {
+// //       console.error(e.message);
+// //     }
+// //   });
+// // }).on('error', (e) => {
+// //   console.error(`Got error: ${e.message}`);
+// // });
+
+
+// // const https = require('https');
+
+// // https.get('http://superpatanegra.com/texto/index.php', (resp) => {
+// //   let data = '';
+
+// //   // A chunk of data has been recieved.
+// //   resp.on('data', (chunk) => {
+// //     data += chunk;
+// //   });
+
+// //   // The whole response has been received. Print out the result.
+// //   resp.on('end', () => {
+// //     console.log(JSON.parse(data).explanation);
+// //   });
+
+// // }).on("error", (err) => {
+// //   console.log("Error: " + err.message);
+// // });
+
+// // // const https = require('https')
+// // // const options = {
+// // //   hostname: 'flaviocopes.com',
+// // //   port: 443,
+// // //   path: '/todos',
+// // //   method: 'GET'
+// // // }
+
+// // // const req = https.request(options, (res) => {
+// // //   console.log(`statusCode: ${res.statusCode}`)
+
+// // //   // res.on('data', (d) => {
+// // //   //   process.stdout.write(d)
+// // //   //})
+// // // })
+
+// // // req.on('error', (error) => {
+// // //   console.error(error)
+// // // })
+
+// // // req.end()
+

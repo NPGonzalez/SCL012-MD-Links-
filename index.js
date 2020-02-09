6f670c73b2527886ca1fa979cbc793a31a7e8f48
NPGonzalez <naypili@gmail.com>
2020-02-09 20:43:44 -0300

    Validación del path

--- index.js
+++ index.js
@@ -5,12 +5,18 @@
 //ALMACENAMIENTO DE PATH
 let pathAbsRel = process.argv[2];
 
-
-//ALMACENAMIENTO DEL ACTUAL DIRECTORIO DE TRABAJO
-let relativePath = process.cwd();
-//TRANSFORMACIÓN A RUTA ABSOLUTA
-let absolutePathToken = relativePath + '\\' + pathToken;
-const readPath = (pathName) =>{
-//SE MUESTRA RUTA ABSOLUTA...(
-console.log(pathName);
-};
+// VALIDACIÓN DE PATH
+if(pathToken === undefined){
+  console.log('ingrese ruta')}
+  else{  
+  //ALMACENAMIENTO DEL ACTUAL DIRECTORIO DE TRABAJO
+  let relativePath = process.cwd();
+  
+  //TRANSFORMACIÓN A RUTA ABSOLUTA
+  let absolutePathToken = relativePath + '\\' + pathToken;
+  const readPath = (pathName) =>{
+  //SE MUESTRA RUTA ABSOLUTA...(
+  console.log(pathName);
+  };
+    readPath(absolutePathToken);
+  };

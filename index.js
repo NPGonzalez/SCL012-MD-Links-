0bb8b2c0aa0ee894d11af8311d5d7a7bea26a0cb
NPGonzalez <naypili@gmail.com>
2020-02-10 18:57:14 -0300

    Imprime archivos con extensión .md del current working directory, si ruta vacía

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ const fs = require('fs');
 const mdlinks = (pathToken) => {
 // VALIDACIÓN DE PATH
   if (pathToken === undefined) {
-    console.log('Ingrese ruta');
+    // console.log('Ingrese ruta');
     const relativePath = process.cwd();
     // FUNCIÓN QUE IMPRIME ARCHIVOS QUE SE ENCUENTRAN EN EL CURRENT WORKING DIRECTORY
     fs.readdir(relativePath, (err, files) => {
       files.forEach((file) => {
-        console.log(path.extname(file));
+        if (path.extname(file) === '.md') {
+          console.log(file);
+        };
       });
     });
   } else {

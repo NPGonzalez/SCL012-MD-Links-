374ba293daa4c688d8a4002ba26335ef4700d669
NPGonzalez <naypili@gmail.com>
2020-02-10 18:42:57 -0300

    Lectura de archivos y carpetas del current working directory, si ruta vacía

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 // //module.exports = () => {
-//   // ...
-//   //}
+//  ...
+// }
+// ALMACENAMIENTO DE PATH
 
-//ALMACENAMIENTO DE PATH
-let pathAbsRel = process.argv[2];
-
+const pathAbsRel = process.argv[2];
+const path = require('path');
+const fs = require('fs');
 // FUNCIÓN mdLinks(path)
-const mdlinks = (pathToken) =>{
-  // VALIDACIÓN DE PATH
-  if(pathToken === undefined){
-    console.log('ingrese ruta')}
-    else{
-      //ALMACENAMIENTO DEL ACTUAL DIRECTORIO DE TRABAJO
-      let relativePath = process.cwd();
-      // IMPRIME EXTENSIÓN DEL ARCHIVO INGRESADO
-      const path = require('path');
-      //TRANSFORMACIÓN A RUTA ABSOLUTA
-      let absolutePathToken = path.resolve(pathToken);
-      const readPath = (pathName) =>{
-        if(path.extname(pathName) === '.md'){
-          console.log(pathName);
-        } else{
-          console.log("Ingrese archivo con extensión .md");
-        }
-        //SE MUESTRA RUTA ABSOLUTA...
-        
-  };
-  readPath(absolutePathToken);
-  };
-  };
-  
-  mdlinks(pathAbsRel);
+const mdlinks = (pathToken) => {
+// VALIDACIÓN DE PATH
+  if (pathToken === undefined) {
+    console.log('Ingrese ruta');
+    const relativePath = process.cwd();
+    // FUNCIÓN QUE IMPRIME ARCHIVOS QUE SE ENCUENTRAN EN EL CURRENT WORKING DIRECTORY
+    fs.readdir(relativePath, (err, files) => {
+      files.forEach((file) => {
+        console.log(file);
+      });
+    });
+  } else {
+    // TRANSFORMACIÓN A RUTA ABSOLUTA
+    const absolutePathToken = path.resolve(pathToken);
+    const readPath = (pathName) => {
+      if (path.extname(pathName) === '.md') {
+        console.log(pathName);
+   
+      } else {
+        console.log('Ingrese archivo con extensión .md');
+      }
+      // SE MUESTRA RUTA ABSOLUTA...
+    };
+    readPath(absolutePathToken);
+  }
+};
+mdlinks(pathAbsRel);

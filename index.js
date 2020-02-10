7fda67a756a6304900eea624e38550812771e802
NPGonzalez <naypili@gmail.com>
2020-02-09 21:18:42 -0300

    Imprime extensión de archivo a analizar

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 //ALMACENAMIENTO DE PATH
 let pathAbsRel = process.argv[2];
+
 // FUNCIÓN mdLinks(path)
 const mdlinks = (pathToken) =>{
   // VALIDACIÓN DE PATH
@@ -13,9 +14,15 @@ const mdlinks = (pathToken) =>{
   //ALMACENAMIENTO DEL ACTUAL DIRECTORIO DE TRABAJO
   let relativePath = process.cwd();
   
+  // IMPRIME EXTENSIÓN DEL ARCHIVO INGRESADO
+  const path = require('path');
+  console.log(path.extname(pathAbsRel));
+
   //TRANSFORMACIÓN A RUTA ABSOLUTA
-  let absolutePathToken = relativePath + '\\' + pathToken;
-  const readPath = (pathName) =>{
+  //let absolutePathToken = relativePath + '\\' + pathToken;
+  let absolutePathToken = path.resolve(pathToken);
+
+    const readPath = (pathName) =>{
   //SE MUESTRA RUTA ABSOLUTA...(
   console.log(pathName);
   };
